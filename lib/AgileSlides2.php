90f2ed8bace04b78ac1aae2ba84c362cc683ac86
Romans Malinovskis <r@agiletech.ie>
2012-08-07 18:57:42 +0100

    fix slider

--- lib/AgileSlides2.php
+++ lib/AgileSlides2.php
@@ -55,6 +55,8 @@ class AgileSlides2 extends View {
 		parent::render();
 	}
 	function setSource($data){
+		$data=array ( 0 => array ( 'id' => '1', 'label' => 'SortMyBooksOnline.com - Online Accounting Software', 'thumb' => 'content/big-smbo.jpg', 'small_thumb' => 'templates/jui/temp/client-thumbs/smbo.jpg', 'short_description' => 'Best online accounting software launched in Ireland. Contains invoices, quotes, payments, time sheets and much more.', 'description' => '<p>Following growing online community, Aisling Software required to build online solution for their desktop software. They&#160;struggled&#160;with different developers before coming Agile.</p><p>Agile took over the project that was one of the most complex financially and technical and delivered amazing solution. While some project functionality was down-scaled, we spent more energy making each component of the project simple, polished and perfect.</p><div class="image clearing"><div class="image-wrap"><img src="/templates/jui/temp/smbo-login.jpg" alt="SMBO Login page" height="481" width="768" /></div><p class="image-comment">&#160;SMBO login page.</p></div><div class="image clearing"><div class="image-wrap"><img src="/templates/jui/temp/smbo-front.jpg" alt="SMBO Front page" height="833" width="768" /></div><p class="image-comment">Front page of SMBO application</p></div>', 'url' => 'smbo', 'www_url' => 'www.sortmybooksonline.com', 'is_show_on_home' => 'Y', 'ord' => '88', 'html_title' => 'SortMyBooksOnline.com - Online Accounting Software', 'html_descr' => 'SortMyBooksOnline.com is finally launched after 1 year of hard work. It includes much more than any other accounting software.', 'html_keywords' => 'year end, audit log, customers, suppliers, fixed assets, reconciliation, vat periods, journals, jobs, projects, online accounting software, sortmybooksonline, smbo, accounting, time sheets, invoices, quotes, reports, payments, realex, bank accounts', ), 1 => array ( 'id' => '3', 'label' => 'Realex Developer\'s Resource Centre', 'thumb' => 'content/big-realex.jpg', 'small_thumb' => 'templates/jui/temp/client-thumbs/realex.jpg', 'short_description' => 'Realex Payments portal to provide technical information required to integrate with Realex containing list of user guides, developer guides, sample code and documentation.', 'description' => '<p>Realex Payments needed to have a more solid framework for their Resource Centre, CMS had to be integrated with their documentation collections and user access control.&#160;</p><p>Agile delivered portal built on top of Agile Toolkit that looked exactly how Realex Payments required while at the same time added functionality for:</p><ul><li>Documentation listing with download options</li><li>Code sample listings with syntax highlighting</li><li>Page-level security restrictions</li><li>Clean registration form</li><li>Forum and Live Chat integration</li><li>Structured CMS system for static pages</li><li>Blog syndication, Inside News and more</li></ul><div class="image clearing"><div class="image-wrap"><img src="/templates/jui/content/case-realex.jpg" alt="Realex Payments" /></div></div>', 'url' => 'realex', 'www_url' => 'resourcecentre.realexpayments.com', 'is_show_on_home' => 'Y', 'ord' => '90', 'html_title' => 'Realex Developer Resource Centre', 'html_descr' => 'New developer portal for Realex payments is now launched. User guides, documentation, shopping carts, news - all in one place', 'html_keywords' => 'developer portal, realex payments, realex, resource centre, user guide, documentation, shopping card, news, blogs', ), 2 => array ( 'id' => '2', 'label' => 'Agile is 2 Years', 'thumb' => 'content/big-agile2y.jpg', 'small_thumb' => 'templates/jui/content/small-agile2y.jpg', 'short_description' => 'Welcome to the party. Grab a slice of cake and relax while you watch the videos we have prepared.', 'description' => '<p>Welcome to the party. Grab a slice of cake and relax while you watch the <a href="http://www.agiletech.ie/newsletter/2009-10/" title="2y anniversary">videos we have prepared</a>.</p><div class="image clearing"><div class="image-wrap"><img src="/templates/jui/content/case-agile2y.jpg" alt="2 Year Anniversary" /></div></div>', 'url' => '2y', 'www_url' => 'www.agiletech.ie/2y', 'is_show_on_home' => 'Y', 'ord' => '93', 'html_title' => 'Agile 2nd Anniversary', 'html_descr' => 'Agile is now 2 years old. Graba slice of cake and celebrate with us', 'html_keywords' => 'job portal, online accounting software, saas, mapping solution, podcasts, domain portal,mypdocast, epicenter, locle, smbo, sortmybooksonline, gradpool, anniversary, agile technologies, agiletech', ), );
+
 		$this->data=$data;
 
 		$this->step=$this->step% count($this->data);
@@ -84,25 +86,3 @@ class AgileSlides2 extends View {
 	}
 }
 
-
-/*
-class SlidesBullets extends CompleteLister {
-	public $safe_html_output=false;
-	function init(){
-		parent::init();
-		$arrBullets = Array();
-		foreach($this->data as $k=>$d){
-			$arrBullets[$k] = Array(
-				'href'  => $this->api->getDestinationURL(null,array($this->name=>$k)),
-				'class' => "",
-			);
-			if($_GET[$this->name]==$k){
-				$arrBullets[$k]['class'] => ' class="corrent"'
-			}
-		}
-
-		
-		$this->setStaticSource($arrBullets);
-	}
-}
-*/
--- page/index.php
+++ page/index.php
@@ -1,28 +1,16 @@
-<?
-class page_index extends AWPage {
+<?php
+class page_index extends Page {
 	function init(){
 		parent::init();
 		// $this->add('AgileSlides',null,'Slides','Slides')
 			// ->setSource($this->api->front_slides)
 			// ;
 			
-		$modelPortfolio = $this->add('Model_Portfolio');
-		$arrData        = $modelPortfolio->getByHomePage();
-		
-		// var_dump($arrData);
-		// var_dump($this->api->front_slides);
 			
 		$slides = $this->add('AgileSlides2',null,'Slides2','Slides2')
 			// ->setSource($this->api->front_slides)
-			->setSource($arrData)
+			->setSource(array('foo','bar','baz'));
 			;
-			
-		if ($this->api->auth->isLoggedIn()){
-			$slides->add('AWEditable')->enableAdmin('admin/portfolio');
-		}
-			
-		$b = $this->add('LastBlogRow', 'LastBlogRow', 'LastBlogRow', 'LastBlogRow');
-		$b->add('AWEditable')->enableAdmin('admin/blog');
 
 		$this->js('click')->_selector('.newsletter')
 			->univ()->dialogURL('Newsletter',$this->api->getDestinationURL('newsletter.html'),
@@ -32,26 +20,5 @@ class page_index extends AWPage {
 	}
 	function defaultTemplate(){
 		return array('page/index','_top');
-	}
-}
-
-class LastBlogRow extends CompleteLister {
-	public $safe_html_output=false;
-	function init(){
-		parent::init();
-			
-		$modelBlog = $this->add('Model_Blog');
-		$arrData   = $modelBlog->getLastRow();
-		
-		// var_dump($arrData);
-		
-		if ($arrData['is_show_comments'] != 'Y'){
-			$arrData['CommentContainer'] = '';
-		}
-		
-		// print_r($arrData);
-		
-		$this->setStaticSource(Array($arrData));
-
 	}
 }
