094a26b91182464e14b92ef655ada377d0d4ed67
Romans Malinovskis <r@agiletech.ie>
2013-09-25 16:08:50 +0100

    make use of layouts.

--- lib/Frontend.php
+++ lib/Frontend.php
@@ -25,12 +25,17 @@ class Frontend extends ApiFrontend {
     }
 
     function initLayout(){
-        $m = $this->add('Menu', 'Menu', 'Menu');
+
+        $l = $this->add('Layout_Fluid');
+
+        $m = $l->addMenu('Menu');
         $m->addMenuItem('index','Home');
         $m->addMenuItem('services','Services');
         $m->addMenuItem('team','Team');
         $m->addMenuItem('portfolio','Portfolio');
         $m->addMenuItem('contact','Contact');
+
+        $l->addFooter('Text')->setHTML('&copy; 2003-2013 Agile55');
 
         parent::initLayout();
     }
