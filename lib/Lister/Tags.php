3a5eea2c045ac7641accbc55862be49c2630f330
Romans Malinovskis <r@agiletech.ie>
2013-09-25 15:10:42 +0100

    add highlighting of current tag

--- lib/Lister/Tags.php
+++ lib/Lister/Tags.php
@@ -6,6 +6,8 @@ class Lister_Tags extends CompleteLister {
     function formatRow(){
         parent::formatRow();
 
+        $this->current_row['effect']=$_GET['tag']==$this->current_row['name']?'success':'info';
+
         //$this->current_row_html['link']=$this->api->url(null,array('tag'=>$this->current_row['link']));
     }
     function defaultTemplate(){
