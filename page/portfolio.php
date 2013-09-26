1c163cfc2ce76ed301f1d3853a702b24463d2104
Romans Malinovskis <r@agiletech.ie>
2013-09-26 08:35:32 +0100

    add project descriptions.

--- page/portfolio.php
+++ page/portfolio.php
@@ -78,58 +78,105 @@ class page_portfolio extends Page {
     }
 
     protected $projects=array(
+        // most important
         array(
-            'name'=>'3rd Chamber',
-            'link'=>'portfolio/3rdchamber',
-            'image'=>'images/portfolio/3rd_chamber.png',
-            'tags'=>array('business','test'),
-        ),
-        array(
-            'name'=>'X-Factor',
+            'name'=>'The X-Factor - Fame Game',
             'link'=>'portfolio/xfactor',
             'image'=>'images/portfolio/small-xfactor.jpg',
-            'tags'=>array('business','test2'),
+            'tags'=>array('development','design','hosting'),
+            'descr'=>'We were contracted to develop a Online RPG game for a major entertainment brand. During period of three months we have developed a fully-functional HTML5-based multiplayer roleplayer game.'
         ),
         array(
-            'name'=>'My-Tools',
-            'link'=>'portfolio/mytools',
-            'image'=>'images/portfolio/small-mytools.jpg',
-            'tags'=>array(),
+            'name'=>'AppsFuel',
+            'link'=>'portfolio/appsfuel',
+            'image'=>'images/portfolio/small-appsfuel.jpg',
+            'tags'=>array('design','training'),
+            'descr'=>'AppsFuel is a platform developed and owned by Docomo Japan. We were contracted to provide a commercial Agile Toolkit support. With our help, a team of developers successfully adopted Agile Toolkit for one of the biggest on-line projects run by Docomo.'
         ),
         array(
-            'name'=>'Ven',
-            'link'=>'portfolio/ven',
-            'image'=>'images/portfolio/small-ven.jpg',
-            'tags'=>array(),
-        ),
-        array(
-            'name'=>'zoubizous',
-            'link'=>'portfolio/zoubizous',
-            'image'=>'images/portfolio/zoubizous-profile.png',
-            'tags'=>array(),
+            'name'=>'SortMyBooks Online',
+            'link'=>'portfolio/smbo',
+            'image'=>'images/portfolio/smbo.jpg',
+            'tags'=>array('development','design','hosting'),
+            'descr'=>'A small development company in Ireland contracted us to convert their desktop accounting application into On-line software. By worked together we have built an on-line accounting software which is considered to be one of the most simple-to-use on-line accounting packages for the past 4 years.'
         ),
         array(
             'name'=>'Elexu.com',
             'link'=>'portfolio/elexu',
             'image'=>'images/portfolio/elexu-profile.png',
-            'tags'=>array(),
+            'tags'=>array('training','developent','design'),
+            'descr'=>'Elexu is an startup working to build a new generation of interractive social television. Our team have developed the initial prototypes and helped to hire and train in-house developers.'
         ),
         array(
-            'name'=>'whitepier',
-            'link'=>'portfolio/whitepier',
-            'image'=>'images/portfolio/whitepier.png',
-            'tags'=>array(),
+            'name'=>'Ven',
+            'link'=>'portfolio/ven',
+            'image'=>'images/portfolio/small-ven.jpg',
+            'tags'=>array('development','design','hosting','training'),
+            'descr'=>'HubCulture is a company operating one of the major virtual currencies - Ven. Our team have built a scalable architecture for a next generation of Ven financial framework. Not often PHP is used for high-end financial systems, but with Agile Toolkit the system turned out to be efficient, stable and secure'
         ),
         array(
-            'name'=>'yapzar',
-            'link'=>'portfolio/yapzar',
-            'image'=>'images/portfolio/yapzar.png',
-            'tags'=>array(),
+            'name'=>'Linked Finance',
+            'link'=>'portfolio/linkedfinance',
+            'image'=>'images/portfolio/small-linkedfinance.jpg',
+            'tags'=>array('development','hosting'),
+            'descr'=>'Yet another on-line financial project - Linked Finance is a peer-to-peer investment platform. Operating large amount of micro-investments this project rivals Kickstarter in its sophistication. Again - Agile Tookit have excelled at doing pristine job at getting the numbers to match'
         ),
         array(
             'name'=>'surf-accounts',
             'link'=>'portfolio/surfaccounts',
             'image'=>'images/portfolio/surf-accounts.png',
+            'tags'=>array('design'),
+            'descr'=>'SurfAccounts is one of the rare projects in which we did not develop but only contributed the design. A sophisticated and well-polished user interface was developed to make this on-line accounting and payroll solution easy to use'
+        ),
+        array(
+            'name'=>'Realex',
+            'link'=>'portfolio/realex',
+            'image'=>'images/portfolio/realex.jpg',
+            'tags'=>array('development'),
+            'descr'=>'A largest european credit card merchant hired our team to build their Developer Resource Center.'
+        ),
+
+        // Less important clients
+        array(
+            'name'=>'Gradpool',
+            'link'=>'portfolio/gradpool',
+            'image'=>'images/portfolio/gradpool.jpg',
+            'tags'=>array('startup','development','design','hosting'),
+            'descr'=>'Gradpool is a start-up aiming at helping out graduate students to find an appropriate internship program. We have helped to develop the initial prototype of the application and then refactor it entirely. Only the flexibility of Agile Toolkit would have alowed us to adopt quicky to the changing business requirements'
+        ),
+        array(
+            'name'=>'3rd Chamber',
+            'link'=>'portfolio/3rdchamber',
+            'image'=>'images/portfolio/3rd_chamber.png',
+            'tags'=>array('startup','development','design','hosting'),
+            'descr'=>'An interesting idea on how to improve poitical structure in UK sparked this curious project. Agile Toolkit allowed us to build an initial prototype very quickly and later add design on top of existing business logic.',
+        ),
+        array(
+            'name'=>'My-Tools',
+            'link'=>'portfolio/mytools',
+            'image'=>'images/portfolio/small-mytools.jpg',
+            'tags'=>array('development','design','hosting'),
+            'descr'=>'While it appears that My-Tools is just another on-line store, it is actually a highly custom commerce site. It is integrated with an extensive management software from jPoint and is actually one of the biggest on-line stores in Ireland'
+        ),
+
+
+        array(
+            'name'=>'zoubizous',
+            'link'=>'portfolio/zoubizous',
+            'image'=>'images/portfolio/zoubizous-profile.png',
+            'tags'=>array('development','design','discontinued'),
+            'descr'=>'We have started to develop this project as a joint venture with a private investor, but had to stop after the initial build due to funding problems. We agreed to open-source the project and use it as a tutorial for anyone who wants to earn Agile Toolkit'
+        ),
+        array(
+            'name'=>'whitepier',
+            'link'=>'portfolio/whitepier',
+            'image'=>'images/portfolio/whitepier.png',
+            'tags'=>array('development','design','discontinued'),
+        ),
+        array(
+            'name'=>'yapzar',
+            'link'=>'portfolio/yapzar',
+            'image'=>'images/portfolio/yapzar.png',
             'tags'=>array(),
         ),
         array(
@@ -151,12 +198,6 @@ class page_portfolio extends Page {
             'tags'=>array(),
         ),
         array(
-            'name'=>'Linked Finance',
-            'link'=>'portfolio/linkedfinance',
-            'image'=>'images/portfolio/small-linkedfinance.jpg',
-            'tags'=>array(),
-        ),
-        array(
             'name'=>'LimsABC',
             'link'=>'portfolio/lims',
             'image'=>'images/portfolio/small-lims.jpg',
@@ -166,12 +207,6 @@ class page_portfolio extends Page {
             'name'=>'Job Tracker',
             'link'=>'portfolio/jobtracker',
             'image'=>'images/portfolio/small-jobtracker.jpg',
-            'tags'=>array(),
-        ),
-        array(
-            'name'=>'Apps Fuel',
-            'link'=>'portfolio/appsfuel',
-            'image'=>'images/portfolio/small-appsfuel.jpg',
             'tags'=>array(),
         ),
         array(
@@ -211,18 +246,6 @@ class page_portfolio extends Page {
             'tags'=>array(),
         ),
         array(
-            'name'=>'SortMyBooksOnline',
-            'link'=>'portfolio/smbo',
-            'image'=>'images/portfolio/smbo.jpg',
-            'tags'=>array(),
-        ),
-        array(
-            'name'=>'Realex',
-            'link'=>'portfolio/realex',
-            'image'=>'images/portfolio/realex.jpg',
-            'tags'=>array(),
-        ),
-        array(
             'name'=>'Epicenter',
             'link'=>'portfolio/epicenter',
             'image'=>'images/portfolio/epicenter.jpg',
@@ -238,12 +261,6 @@ class page_portfolio extends Page {
             'name'=>'Eircom Locle',
             'link'=>'portfolio/locle',
             'image'=>'images/portfolio/locle.jpg',
-            'tags'=>array(),
-        ),
-        array(
-            'name'=>'Gradpool',
-            'link'=>'portfolio/gradpool',
-            'image'=>'images/portfolio/gradpool.jpg',
             'tags'=>array(),
         ),
         array(
