f70d870b5a29fdfbf0ef127660a16c8fe8ac5517
Romans Malinovskis <r@agiletech.ie>
2013-10-03 00:11:44 +0100

    add Commas.

--- lib/Lister/Tags.php
+++ lib/Lister/Tags.php
@@ -3,10 +3,13 @@ class Lister_Tags extends CompleteLister {
 	function init(){
 		parent::init();
 	}
+    public $first=true;
     function formatRow(){
         parent::formatRow();
 
         $this->current_row['effect']=$_GET['tag']==$this->current_row['name']?'success':'info';
+
+        if($this->first) $this->first=false; else $this->current_row['comma'] = ', ';
 
         //$this->current_row_html['link']=$this->api->url(null,array('tag'=>$this->current_row['link']));
     }
